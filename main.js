39d380125ee71278d3acdf0593e9549582e89193
halftimegoall <halftimegoall@gmail.com>
2026-03-11 09:30:05 +0000

    feat: implement Lotto Number Drawer

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
-// Add JS here
+const lottoNumbers = document.querySelector(".lotto-numbers");
+const drawButton = document.querySelector("button");
+
+function paintNumber(number) {
+  const lottoNumber = document.createElement("div");
+  lottoNumber.classList.add("lotto-number");
+  lottoNumber.textContent = number;
+  lottoNumbers.appendChild(lottoNumber);
+}
+
+function paintNumbers() {
+  lottoNumbers.innerHTML = "";
+  const numbers = [];
+  while (numbers.length < 6) {
+    const number = Math.floor(Math.random() * 45) + 1;
+    if (!numbers.includes(number)) {
+      numbers.push(number);
+    }
+  }
+  numbers.forEach(paintNumber);
+}
+
+drawButton.addEventListener("click", paintNumbers);
